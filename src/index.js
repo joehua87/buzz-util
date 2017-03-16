7fe4bcc07d78ee974c54eb3b133b1cfd421c84eb
joehua87 <joehua87@gmail.com>
2017-03-16 14:22:28 +0700

    export to index

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import outputData from './outputData'
 import readFromCsv from './readFromCsv'
 import insertIfNotExists from './insertIfNotExists'
 import upsert from './upsert'
+import createMongoUri from './createMongoUri'
 
 export {
   outputData,
   readFromCsv,
   insertIfNotExists,
   upsert,
+  createMongoUri,
 }
